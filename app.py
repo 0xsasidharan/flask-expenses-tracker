47632701cd76420e486a41934773250696801ec0
Sasi <sasidharan0x@gmail.com>
2025-07-23 22:14:13 +0530

    Sqlite DB init

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 from flask import Flask
 from flask_smorest import Api
 from resources.expense import blp as ExpensesBluePrint
+from db import db
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Expenses tracker REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Expenses tracker REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-api = Api(app)
 
-api.register_blueprint(ExpensesBluePrint)
+    db.init_app(app)
+    api = Api(app)
 
+    api.register_blueprint(ExpensesBluePrint)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 
 
--- db.py
+++ db.py
@@ -1,14 +1,18 @@
-expenses = {
-    # "1" : {
-    #     "expense_id" : "1" ,
-    #     "amount" : 10,
-    #     "category" : "food" ,
-    #     "description" : "Spent for the lunch."
-    # } ,
-    # "2" : {
-    #     "expense_id" : "2" ,
-    #     "amount" : 15,
-    #     "category" : "travel" ,
-    #     "description" : "Bus fare."
-    # }
-}
+# expenses = {
+#     # "1" : {
+#     #     "expense_id" : "1" ,
+#     #     "amount" : 10,
+#     #     "category" : "food" ,
+#     #     "description" : "Spent for the lunch."
+#     # } ,
+#     # "2" : {
+#     #     "expense_id" : "2" ,
+#     #     "amount" : 15,
+#     #     "category" : "travel" ,
+#     #     "description" : "Bus fare."
+#     # }
+# }
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
